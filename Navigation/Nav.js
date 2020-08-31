f0b2b6f3f06f91e4eb7c8d409c1d86f0eeacdcfe
Yannis <yannis.brudey@icloud.com>
2020-08-31 16:28:03 +0200

    first commit

---
+++ Navigation/Nav.js
@@ -0,0 +1,20 @@
+// Navigation/Nav.js
+
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
+
+const SearchStackNavigator = createStackNavigator({
+    Search: {
+        screen: Search,
+        navigationOptions: {
+            title: 'Rechercher'
+        }
+    },
+    FilmDetail: {
+        screen: FilmDetail
+    }
+})
+
+export default createAppContainer(SearchStackNavigator)
